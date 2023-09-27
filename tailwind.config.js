2d627f70a5a252c53a49bdc10ce4fb04d1c2fbf8
Haghest <haghest@gmail.com>
2023-09-28 06:28:01 +0800

    master

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./components/**/*.{html,js}", "./pages/**/*.{html,js}", "./index.html", "./index2.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
