d8cc1a3f9960d9f98073308c2cffba5d3709b9f5
Haghest <haghest@gmail.com>
2023-09-28 20:41:45 +0800

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: "class",
-  content: ["./components/**/*.{html,js}", "./pages/**/*.{html,js}", "./index.html", "./index2.html"],
+  content: ["./components/**/*.{html,js}", "./pages/**/*.{html,js}", "./index.html", "./index2.html", "node_modules/preline/dist/*.js"],
+
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("preline/plugin")],
 };
